a3e8627ca873490622abaca3d951ead866aec74e
sidsun1 <siddharthsundar2@gmail.com>
2025-04-17 17:12:54 -0700

    Added random image selection

--- backend/server_connection.cpp
+++ backend/server_connection.cpp
@@ -1,12 +1,28 @@
 #include "crow.h"
+#include <map>
+#include <string>
 
 int main() {
+    using namespace std;
+
     crow::App<crow::CORSHandler> app;
     auto& cors = app.get_middleware<crow::CORSHandler>().global();
     cors.origin("*").methods("GET"_method);
 
+
+
     CROW_ROUTE(app, "/home").methods("GET"_method) ([](){
-        return "UCI Geoguesser!";
+
+        // text rn, will have to probably have some id and then if we're storing images on a db
+        // just match it and query for that specific one
+        map<string, string> images = {{"1" , "Image 1"}, {"2" , "Image 2"}, {"3" , "Image 3"}, {"4" , "Image 4"}};
+        
+        crow::json::wvalue res;
+        
+        for(auto& i : images){
+            res["images"][i.first] = i.second;
+        }
+        return res;
     });
 
     app.port(18080).multithreaded().run();
--- frontend/ucigeoguesser/app/page.tsx
+++ frontend/ucigeoguesser/app/page.tsx
@@ -5,18 +5,25 @@ import React, { useState, useEffect } from 'react';
 // import tailwindcss from '@tailwindcss/postcss';
 
 const App = () => {
-  const [msg, setMsg] = useState('Testing..');
+  const [msg, setMsg] = useState<string>('Testing..');
 
   useEffect(() => {
     fetch('http://localhost:18080/home')
-      .then(res => res.text())
-      .then(text => setMsg(text))
+      .then(res => res.json())
+      .then(text => {
+        const images = text.images;
+        const imagesKeys = Object.keys(images);
+        const randomKey = imagesKeys[Math.floor(Math.random() * imagesKeys.length)]
+        console.log(randomKey);
+        console.log(images[randomKey]);
+        setMsg(images[randomKey]);
+      })
       .catch(err => setMsg('Error; server didn\'t connect: ' + err));
   }, []);
 
   return(
       <div> 
-        <h1>Server Info</h1>
+        <h1 className=''>Server Info</h1>
         <p>
           {msg}
         </p>
