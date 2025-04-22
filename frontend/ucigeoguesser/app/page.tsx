2e67712b75d8be79ac0e767b3261455d34eefbb6
sidsun1 <siddharthsundar2@gmail.com>
2025-04-22 10:47:02 -0700

    Added method to retrieve coordinates from the backend- conditionally displayed in frontend for now

--- frontend/ucigeoguesser/app/page.tsx
+++ frontend/ucigeoguesser/app/page.tsx
@@ -3,20 +3,28 @@
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [loading, setLoading] = useState<boolean>(true);
+
   const [imageSrc, setImageSrc] = useState<string>('');
+  const [locationData, setLocationData] = useState<string[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:18080/home')
       .then(res => res.json())
       .then(data => {
         const images = data.images;
-        const keys = Object.keys(images);
-        const randomKey = keys[Math.floor(Math.random() * keys.length)];
-        setImageSrc(images[randomKey]);
+        const imageKey = Object.keys(images);
+        const randomKey = imageKey[Math.floor(Math.random() * imageKey.length)];
+        setImageSrc(images[randomKey]['image']);
+        setLocationData([images[randomKey]['metadata']['geoData']['longitude'], images[randomKey]['metadata']['geoData']['latitude']])
+        setLoading(false);
       })
       .catch(err => {
         console.error(err);
         setImageSrc('');
+        setLocationData([]);
+        setLoading(true);
       });
   }, []);
 
@@ -34,6 +42,14 @@ const App = () => {
         <h1 className="mb-4 text-black font-extrabold text-4xl">
           UCI GeoGuessr
         </h1>
+        {loading ? <p>Loading Coordinates...</p> : (
+          locationData && (
+          <h2 className="mb-4 text-slate font-semibold text-3xl">
+            Latitude: { locationData[0]}, Longitude: { locationData[1] }
+          </h2>
+          )
+          )
+          }
         <p className="mt-4 text-xs text-slate-500 font-medium">
           Refresh to get a new location!
         </p>
