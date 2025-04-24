6920fad679fefab10d1de1966ec0d12a71171577
samuelzubin <szubin06@gmail.com>
2025-04-24 12:04:51 -0700

    Changed score calculation

--- frontend/ucigeoguesser/app/score.tsx
+++ frontend/ucigeoguesser/app/score.tsx
@@ -1,14 +1,25 @@
 function calculateScore(userLatitude: number, userLongitude: number, correctLatitude: number, correctLongitude: number) {
     const latScore = Math.abs(userLatitude - correctLatitude);
     const longScore = Math.abs(userLongitude - correctLongitude);
-    
     const distance = Math.sqrt(latScore**2 + longScore**2);
-
     const maxScore = 5000; 
-    let score = Math.round(maxScore * (Math.E ** (-distance*150)));
+    
+    let score: number;
+    
+    if (distance <= 0.00398543) {
+        score = Math.round(-maxScore * Math.E ** ((distance - 0.005) * 650) + 5200);
+    }
 
-    if(score >= 4930){
+    else {
+        score = Math.round(maxScore * Math.E ** (-658 * (distance - 0.003)))
+    }
+
+    if(score >= 4950){
         score = 5000;
+    }
+
+    if (score < 0) {
+        score = 0;
     }
 
     return score;
